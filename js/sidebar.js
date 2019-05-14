0bfcf88c556946dcb7a6b7860af889c1249820dc
ragnals <ragnals@stud.ntnu.no>
2019-05-14 21:11:35 +0200

    yey

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,11 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+  document.getElementById("sideNavbarC").style.width = "290px";
+  document.getElementById("darkOverlay").style.width = "100%";
+}
+/* Set the width of the sidebar to 0 */
+function closeNav() {
+  document.getElementById("sideNavbarC").style.width = "0";
+  document.getElementById("darkOverlay").style.width = "0";
+
+}
